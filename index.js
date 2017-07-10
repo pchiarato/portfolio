afad1e93d9e9d7c3f4c1fd37af903286be4325ce
pchiarato <phillip.chiarato@gmail.com>
2017-07-10 19:10:33 -0400

    fixed typos

--- index.js
+++ index.js
@@ -9,5 +9,5 @@ app.use(express.static(path.join(__dirname, 'dist')))
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'dist/index.html'))
 });
-let port = 8082
+let port = 3001
 app.listen(port,() => console.log(`lisening on port ${port}`))
